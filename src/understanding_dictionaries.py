f6c11ac25a521fb6625c9a9df1d9bfb2a1dc7bcd
Miguel-Enrique-Esquivel-Garcia <2530031@upv.edu.mx>
2025-11-19 08:44:32 -0600

    dictionaries

---
+++ src/understanding_dictionaries.py
@@ -0,0 +1,22 @@
+# Simple dictionary
+alien_0 = {'color':'green','points': 5} #un diccionario tiene llave : valor
+print(alien_0['color'])
+
+# The simplest dictionary
+alien_1 = {'color':'yellow'}
+
+#Accessing values in the dictionary
+print(alien_0['points'])
+print(alien_1['color'])
+
+#Empty dictionary
+alien_2 = {}
+#Modifying values in a dictionary
+alien_2 = {'color':'yellow'}
+alien_2 ['color'] = 'blue'
+
+# Adding new key-value
+alien_2['x_position'] = 0
+alien_2['y_position'] = 25
+
+print(alien_2)
