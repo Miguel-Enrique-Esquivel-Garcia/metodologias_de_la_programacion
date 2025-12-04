95c1761177b585ea24ab1141a0f0d71120a0d68f
Miguel-Enrique-Esquivel-Garcia <2530031@upv.edu.mx>
2025-12-03 23:16:06 -0600

    almost crud

--- src/Homework/CRUD_in_Python/2530031_EsquivelMiguel_t_crud.py
+++ src/Homework/CRUD_in_Python/2530031_EsquivelMiguel_t_crud.py
@@ -48,16 +48,46 @@ usando funciones para cada operación y un menú de texto para interactuar con e
 
 print("Problem: Gestor CRUD usando diccionarios y/o listas con funciones.")
 def create_item(data_structure, item_id, name, price, quantity):
-    return:
+    if item_id not in data_structure and item_id != "":
+        if name != "" and price >= 0.0 and quantity >= 0:
+            data_structure[item_id] = [f"Name: {name}", f"Price: {price}", f"Quantity: {quantity}"]
+            print(data_structure)
+            return True, "Item created"
+        else:
+            return False, "Error: invalid input"
+    else:
+        return False, "The item already exists"
 def read_item(data_structure, item_id):
-    return:
+    if item_id in data_structure:
+        return f"For Item ID {item_id} : {data_structure.get(item_id)}"
+    else:
+        return None
 def update_item(data_structure, item_id, new_name, new_price, new_quantity):
-    return:
+    if item_id in data_structure and item_id != "":
+        if new_name != "" and new_price >= 0.0 and new_quantity >= 0:
+            data_structure[item_id] = [f"Name: {new_name}", f"Price: {new_price}", f"Quantity: {new_quantity}"]
+            print(data_structure)
+            return True, "Item updated"
+        else:
+            return False, "Error: invalid input"
+    else:
+        return False, "The item not exist"
 def delete_item(data_structure, item_id):
-    return:
+    if item_id in data_structure and item_id != "":
+        del data_structure[item_id]
+        return True, "Item deleted"
+    else:
+        return False, "The item not exist"
 def list_items(data_structure):
-    return:
-almacenated_items = {}
+    if data_structure == {}:
+        return "No items found"
+    else:
+        print("Items list:")
+        for item_id, item in data_structure.items():
+            print(f"ID: {item_id}, item : {item}")
+        return ""
+
+data_structure = {}
 while True:
     print("1) Create item")
     print("2) Read item by id")
@@ -66,12 +96,54 @@ while True:
     print("5) List all items")
     print("0) Exit")    
     try:
-        option = input("Choose an option: ")
+        option = input("Choose an option: ").strip()
         if int(option) == 0:
+            print("Exit")
             break
         else:
             if int(option) == 1:
-                print("Simóm")
+                print("Create an item: ")
+                item_id = input("Set the id for the item: ").strip()
+                name = input("Set the item's name: ").lower().strip()
+                price = float(input("Set the item's price: ").strip())
+                quantity = int(input("Set the item's quantity: ").strip())
+                quest,text = create_item(data_structure, item_id, name, price, quantity)
+                if quest == True:
+                    print(text)
+                else:
+                    print(text)
+            elif int(option) == 2:
+                print("Read item by id: ")
+                item_id = input("Set the id for the item: ").strip()
+                read = read_item(data_structure, item_id)
+                if read == None:
+                    print ("Item not found")
+                else:
+                    print(read)
+            elif int(option) == 3:
+                print("Update item by id: ")
+                item_id = input("Set the id for the item: ").strip()
+                new_name = input("Set the item's new name: ").lower().strip()
+                new_price = float(input("Set the item's new price: ").strip())
+                new_quantity = int(input("Set the item's new quantity: ").strip())
+                questi,textt = update_item(data_structure, item_id, new_name, new_price, new_quantity)
+                if questi == True:
+                    print(textt)
+                    print(data_structure)
+                else:
+                    print(textt)
+            elif int(option) == 4:
+                print("Delete item by id: ")
+                item_id = input("Set the id for the item: ").strip()
+                questu,textx = delete_item(data_structure, item_id)
+                if questu == True:
+                    print(textx)
+                    print(data_structure)
+                else:
+                    print(textx)
+            elif int(option) == 5:
+                print(list_items(data_structure))
+
     except:
         print("Error: invalid input")
         
